c256af3e09d84a278bb09a3f709e41328a016093
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2023-12-07 10:27:33 +0100

    Create Calculator.java
    started work on the Calculator

---
+++ src/main/Java/app/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package app.calculator;
+
+public class Calculator {
+
+    public int getPillers(){
+        int neededPillers = 0;
+        return neededPillers;
+    }
+    public int getPillers(){
+        int neededPillers = 0;
+        return neededPillers;
+    }
+    public int getPillers(){
+        int neededPillers = 0;
+        return neededPillers;
+    }
+}
