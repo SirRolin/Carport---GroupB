e142b6808fcf3d96450621986da1e12ee0755780
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2023-12-13 14:28:42 +0100

    setup the shell for out orderEditSite

--- src/main/Java/app/controllers/OrderEditController.java
+++ src/main/Java/app/controllers/OrderEditController.java
@@ -5,7 +5,15 @@ import io.javalin.http.Context;
 
 public class OrderEditController {
     public static void loadOrderEditSite(Context ctx, ConnectionPool connectionPool){
+        ctx.sessionAttribute("chosen_order",null);
+        ctx.sessionAttribute("costumer_orders",null);
 
         ctx.render("orderEditSite.html");
+    }
+    public static void loadOrderWithID(Context ctx,ConnectionPool connectionPool){
+
+    }
+    public static void showOrderWithID(Context ctx,ConnectionPool connectionPool){
+
     }
 }
