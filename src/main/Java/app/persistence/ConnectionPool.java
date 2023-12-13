ffee9645296edae6fe7f8f6a0ea4cd498f6fd8ad
Nicklas W <Nicklas2002@hotmail.com>
2023-12-13 10:21:44 +0100

    ConnectionPool fix

--- src/main/Java/app/persistence/ConnectionPool.java
+++ src/main/Java/app/persistence/ConnectionPool.java
@@ -15,7 +15,7 @@ public class ConnectionPool {
         private static final String DEFAULT_USER = "postgres";
         private static final String DEFAULT_PASSWORD = "postgres";
         private static final String DEFAULT_URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-        private static final String DEFAULT_DB = "cupcake";
+        private static final String DEFAULT_DB = "carport";
 
         public static ConnectionPool instance = null;
         public static HikariDataSource ds = null;
