c34b58b474dbe146770008b7e356277c70fb976d
chris4567890 <starchristian4@hotmail.com>
2023-12-14 09:40:54 +0100

    greg
    greg

---
+++ src/main/Java/app/entities/PillarDTO.java
@@ -0,0 +1,59 @@
+package app.entities;
+
+import org.jetbrains.annotations.NotNull;
+
+public class PillarDTO extends MaterialDTO implements Comparable<MaterialDTO>{
+
+    private int materialVariantID;
+    private int length;
+    private int price;
+    private int amount = 0;
+    private String unitType = "stk"; // TODO it should not be hard coded
+
+    //int materialId, String name, Mtype type, int widthMm, int depthMm, String description
+   public PillarDTO(int materialID, String name, Mtype type, int amount, String description, int width_mm, int depth_mm, int materialVariantID, int length, int price) {
+        super(materialID, name, type, amount, width_mm, depth_mm,description);
+        this.materialVariantID = materialVariantID;
+        this.length = length;
+        this.price = price;
+    }
+    public PillarDTO(int materialID, String name, Mtype type, String description, int width_mm, int depth_mm, int materialVariantID, int length, int price) {
+        super(materialID, name, type, width_mm, depth_mm, description);
+        this.materialVariantID = materialVariantID;
+        this.length = length;
+        this.price = price;
+    }
+
+    public PillarDTO(int materialId, String name, Mtype type, int widthMm, int depthMm,String description){
+       super(materialId,name,type,widthMm,depthMm,description);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getUnitType() {
+        return unitType;
+    }
+
+    @Override
+    public int getMaterialVariantID() {
+        return materialVariantID;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int compareTo(@NotNull MaterialDTO o) {
+       String strLength = String.valueOf(length);
+       return strLength.compareTo(String.valueOf(o.getLength()));
+    }
+}
